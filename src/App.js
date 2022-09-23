7cf18b5b8e7ea3440151a5f1a9bbf7696d65cdcf
Ben Whitfield <whitfieldbenjamin@gmail.com>
2022-09-23 10:22:10 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
+
+/*
+
+A section to add general information like name, email, phone number. 
+
+A section to add your educational experience (school name, title of study, date
+of study)
+
+A section to add practical experience (company name, position title, main tasks
+of your jobs, date from and until when you worked for that company)
+
+edit and submit button for each section or for the whole CV
+
+The submit button should submit your form and display the
+value of your input fields in HTML elements. 
+
+The edit button should add back (display) the input fields, 
+with the previously displayed information as values.
+
+In those input fields, you should be able to edit and resubmit the content.
+
+add your components. 
+Styles folder - You’ll need to import these in the component files to use them. 
+
+*/
